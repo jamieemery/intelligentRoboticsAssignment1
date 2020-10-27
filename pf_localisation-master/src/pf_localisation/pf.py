64ded90e6780e9540214de05879b0414823e7828
Jamie Emery <d.jamie.emery@gmail.com>
2020-10-27 16:05:16 +0000

    Adding section on updating particle cloud

--- pf_localisation-master/src/pf_localisation/pf.py
+++ pf_localisation-master/src/pf_localisation/pf.py
@@ -39,19 +39,73 @@ class PFLocaliser(PFLocaliserBase):
         """
         pass
 
- 
-    
+
+
     def update_particle_cloud(self, scan):
         """
         Jamie and Traci to do
 
         This should use the supplied laser scan to update the current
         particle cloud. i.e. self.particlecloud should be updated.
-        
+
         :Args:
             | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
 
          """
+
+        """
+        Initialise arrays for the new particle cloud,
+        particle weights and cummulative weights
+        """
+        newParticleCloud = []
+        particleWeights = []
+        cummulativeWeights = []
+
+        """
+        Build the particle weights array using the get weight
+        function provided
+        """
+        for p in self.particlecloud:
+            particleWeights.Add(get_weight(self, scan, p))
+
+        """
+        From the weights calculated create the cummulative
+        weights array needed for the resampling algorithm.
+        If weights for 3 particles were [0.3, 0.5, 0.2] then
+        cummulative weights will be [0.3, 0.8, 1.0]
+        """
+        cummulative = 0
+
+        for w in particleWeights:
+            cummulative += particleWeights[counter]
+            cummulativeWeights.Add(cummulative)
+
+
+        """
+        For the resampling process go through each of the cummulative
+        weights. With each weight add number of particles based on the
+        sampling size (defined as 20 in this case) and the accumulative
+        weight of the particle.
+
+        In example used above it would be 0.3 > 0 add one particle,
+        then check again as while loop 0.3 > 0.05(1/20) and then add
+        another particle. This is repeated until while loop criteria
+        is no longer hit and then it goes onto the next accumulative particle
+        """
+        particleCounter = 0
+        cummulativeMValue = 0
+
+        for cm in cummulativeWeights:
+            while cm > cummulativeMvalue:
+                newParticleCloud.Add(self.particlecloud[particleCounter])
+                cummulativeMValue += 1/self.NUMBER_PREDICTED_READINGS
+            particleCounter += 1
+
+        """
+        Overwrite the old particle cloud with the new resampled one
+        """
+        self.particlecloud = newParticleCloud
+
         pass
 
     def estimate_pose(self):
