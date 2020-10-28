c155e870dc8ab66cae8332a1ad2aff83cac0c5c5
margaritasar <margarita10sar@gmail.com>
2020-10-28 20:29:49 +0000

    If this breaks blame Jamie

--- pf_localisation-master/src/pf_localisation/pf.py
+++ pf_localisation-master/src/pf_localisation/pf.py
@@ -2,6 +2,7 @@ from geometry_msgs.msg import Pose, PoseArray, Quaternion
 from pf_base import PFLocaliserBase
 import math
 import rospy
+import numpy
 
 from util import rotateQuaternion, getHeading
 from random import random
@@ -23,8 +24,6 @@ class PFLocaliser(PFLocaliserBase):
        
     def initialise_particle_cloud(self, initialpose):
         """
-        Liviu to do
-
         Set particle cloud to initialpose plus noise
 
         Called whenever an initialpose message is received (to change the
@@ -43,8 +42,6 @@ class PFLocaliser(PFLocaliserBase):
     
     def update_particle_cloud(self, scan):
         """
-        Jamie and Traci to do
-
         This should use the supplied laser scan to update the current
         particle cloud. i.e. self.particlecloud should be updated.
         
@@ -56,8 +53,6 @@ class PFLocaliser(PFLocaliserBase):
 
     def estimate_pose(self):
         """
-        Marcia and Margarita to do
-
         This should calculate and return an updated robot pose estimate based
         on the particle cloud (self.particlecloud).
         
@@ -72,4 +67,58 @@ class PFLocaliser(PFLocaliserBase):
         :Return:
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
-        pass
+	#remove the outliers, keep the densest particles
+	distances = set();
+	for i = 1:len(self.particlecloud.poses):
+	    p1 = self.particlecloud.poses[i];
+  	    for j = i+1:len(self.particlecloud.poses):
+		p2 = self.particlecloud.poses[j];
+		distances.add(abs(p1.position-p2.position));
+
+	#sort the distances and keep the first half of them	
+	min_dist = sorted(distances)[:math.floor(len(distances)/2)];
+	#calculate each particle's number of appearances in the min_dist
+	counter = numpy.zeros(len(self.particlecloud));	
+        for i = 1:len(self.particlecloud.poses):
+	    p1 = self.particlecloud.poses[i];
+  	    for j = i+1:len(self.particlecloud.poses):
+		p2 = self.particlecloud.poses[j];
+		if abs(p1.position-p2.position) in min_dist:
+		   counter[i-1] += 1;
+		   counter[j-1] += 1;
+
+	
+	#sort counter and keep the particles corresponding to the 2nd half
+	sort_count = sorted(range(len(counter)), key=lambda k: counter[k]);
+	sort_count = sort_count[math.floor(len(sort_count)/2):];
+	wanted_set = set();
+	for i in sort_count:
+	    wanted_set.add(self.particlecloud[i]);
+		
+
+	#find the mean position	
+	x_values = y_values = z_values = 0;
+	for p in wanted_set.poses:
+	    x_values = x_values + p.position.x;
+            y_values = y_values + p.position.y;
+            z_values = z_values + p.position.z;
+	meanX = x_values / len(self.wanted_set.poses);
+	meanY = y_values / len(self.wanted_set.poses);
+	meanZ = z_values / len(self.wanted_set.poses);
+	est_pose.position = [meanX, meanY, meanZ];
+
+
+	#find the mean orientation	
+	x_values = y_values = z_values = w_values = 0;
+	for p in wanted_set.poses:
+	    x_values = x_values + p.orientation.x;
+            y_values = y_values + p.orientation.y;
+            z_values = z_values + p.orientation.z;
+	    z_values = z_values + p.orientation.w;
+	meanX = x_values / len(self.wanted_set.poses);
+	meanY = y_values / len(self.wanted_set.poses);
+	meanZ = z_values / len(self.wanted_set.poses);
+	meanW = w_values / len(self.wanted_set.poses);
+	est_pose.orientation = [meanX, meanY, meanZ, meanW];
+
+        return est_pose
