28ac872015f5660f446b889a839cab765fc715bf
MARCIA_PORTATIL\Marcia <mxf089@student.bham.ac.uk>
2020-11-01 19:36:53 +0000

    Marcia's update to Margarita's code

--- pf_localisation-master/src/pf_localisation/pf.py
+++ pf_localisation-master/src/pf_localisation/pf.py
@@ -11,17 +11,16 @@ from time import time
 
 
 class PFLocaliser(PFLocaliserBase):
-       
+
     def __init__(self):
         # ----- Call the superclass constructor
         super(PFLocaliser, self).__init__()
-        
+
         # ----- Set motion model parameters
- 
+
         # ----- Sensor model parameters
-        self.NUMBER_PREDICTED_READINGS = 20     # Number of readings to predict
-        
-       
+        self.NUMBER_PREDICTED_READINGS = 20  # Number of readings to predict
+
     def initialise_particle_cloud(self, initialpose):
         """
         Set particle cloud to initialpose plus noise
@@ -30,7 +29,7 @@ class PFLocaliser(PFLocaliserBase):
         starting location of the robot), or a new occupancy_map is received.
         self.particlecloud can be initialised here. Initial pose of the robot
         is also set here.
-        
+
         :Args:
             | initialpose: the initial pose estimate
         :Return:
@@ -38,13 +37,11 @@ class PFLocaliser(PFLocaliserBase):
         """
         pass
 
- 
-    
     def update_particle_cloud(self, scan):
         """
         This should use the supplied laser scan to update the current
         particle cloud. i.e. self.particlecloud should be updated.
-        
+
         :Args:
             | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
 
@@ -55,70 +52,84 @@ class PFLocaliser(PFLocaliserBase):
         """
         This should calculate and return an updated robot pose estimate based
         on the particle cloud (self.particlecloud).
-        
+
         Create new estimated pose, given particle cloud
         E.g. just average the location and orientation values of each of
         the particles and return this.
-        
+
         Better approximations could be made by doing some simple clustering,
-        e.g. taking the average location of half the particles after 
+        e.g. taking the average location of half the particles after
         throwing away any which are outliers
 
         :Return:
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
-	#remove the outliers, keep the densest particles
-	distances = set();
-	for i = 1:len(self.particlecloud.poses):
-	    p1 = self.particlecloud.poses[i];
-  	    for j = i+1:len(self.particlecloud.poses):
-		p2 = self.particlecloud.poses[j];
-		distances.add(abs(p1.position-p2.position));
+        # remove the outliers, keep the densest particles
 
-	#sort the distances and keep the first third of them	
-	min_dist = sorted(distances)[:math.ceil(len(distances)/3)-1];
-	#calculate each particle's number of appearances in the min_dist
-	counter = numpy.zeros(len(self.particlecloud));	
-        for i = 1:len(self.particlecloud.poses):
-	    p1 = self.particlecloud.poses[i];
-  	    for j = i+1:len(self.particlecloud.poses):
-		p2 = self.particlecloud.poses[j];
-		if abs(p1.position-p2.position) in min_dist:
-		   counter[i-1] += 1;
-		   counter[j-1] += 1;
+        # compare all the possible distances between particles and "set"
+        # before there was a chance that a single particle is compared with itself
 
-	
-	#sort counter and keep the particles corresponding to the last third
-	sort_count = sorted(range(len(counter)), key=lambda k: counter[k]);
-	sort_count = sort_count[math.ceil(2*len(sort_count)/3)-1:];
-	wanted_set = set();
-	for i in sort_count:
-	    wanted_set.add(self.particlecloud[i]);
-		
+        distances = set()
+        i = 0
+        for p1 in self.particlecloud.poses:
+            i = i + 1
+            for p2 in self.particlecloud.poses[i:]:
+                distance = numpy.sqrt(((p1.position.x - p2.position.x)**2) \
+                                      + ((p1.position.y - p2.position.y)**2) \
+                                      + ((p1.position.z - p2.position.z)**2))
+                distances.add(distance)
 
-	#find the mean position	
-	x_values = y_values = z_values = 0;
-	for p in wanted_set.poses:
-	    x_values = x_values + p.position.x;
-            y_values = y_values + p.position.y;
-            z_values = z_values + p.position.z;
-	meanX = x_values / len(self.wanted_set.poses);
-	meanY = y_values / len(self.wanted_set.poses);
-	meanZ = z_values / len(self.wanted_set.poses);
-	est_pose.position = [meanX, meanY, meanZ];
+        # sort the distances and keep the first third of them
+        min_dist = sorted(distances)[:round(len(distances) / 3)]  # testing !!    !!!!!!!!!!!!!!!
+        # calculate each particle's number of appearances in the min_dist
+        counter = numpy.zeros(len(self.particlecloud))
+        i = 0
+        j = 0
+        # how many particles is a specific particle close to?
+        for p1 in self.particlecloud.poses:
+            i = i + 1
+            j = i - 1
+            for p2 in self.particlecloud.poses[i:]:
+                j = j + 1
+                distance = numpy.sqrt(((p1.position.x - p2.position.x)**2) \
+                                      + ((p1.position.y - p2.position.y)**2) \
+                                      + ((p1.position.z - p2.position.z)**2))
+                if distance in min_dist:
+                    counter[i - 1] += 1
+                    counter[j] += 1
 
 
-	#find the mean orientation	
-	x_values = y_values = z_values = w_values = 0;
-	for p in wanted_set.poses:
-	    x_values = x_values + p.orientation.x;
-            y_values = y_values + p.orientation.y;
-            z_values = z_values + p.orientation.z;
-	    z_values = z_values + p.orientation.w;
-	meanX = x_values / len(self.wanted_set.poses);
-	meanY = y_values / len(self.wanted_set.poses);
-	meanZ = z_values / len(self.wanted_set.poses);
-	meanW = w_values / len(self.wanted_set.poses);
-	est_pose.orientation = [meanX, meanY, meanZ, meanW];
+        # sort counter and keep the particles corresponding to the last third
+        sort_count = sorted(range(len(counter)), key=lambda k: counter[k])
+        sort_count = sort_count[round(2 * len(sort_count) / 3):]
+        wanted_array=[]
+        for i in sort_count:
+            wanted_array=self.particlecloud[i]
+
+        # find the mean position
+        x_values = y_values = z_values = 0
+        for p in wanted_array.poses:
+            x_values += p.position.x     # means -->  x_values = x_values + p.position.x
+            y_values += p.position.y
+            z_values += p.position.z
+
+
+        meanX = x_values / len(self.wanted_array.poses)
+        meanY = y_values / len(self.wanted_array.poses)
+        meanZ = z_values / len(self.wanted_array.poses)
+        est_pose.position = [meanX, meanY, meanZ]
+
+        # find the mean orientation
+        x_values = y_values = z_values = w_values = 0;
+        for p in wanted_array.poses:
+            x_values += p.orientation.x
+            y_values += p.orientation.y
+            z_values += p.orientation.z
+        z_values = z_values + p.orientation.w
+        meanX = x_values / len(self.wanted_array.poses)
+        meanY = y_values / len(self.wanted_array.poses)
+        meanZ = z_values / len(self.wanted_array.poses)
+        meanW = w_values / len(self.wanted_array.poses)
+        est_pose.orientation = [meanX, meanY, meanZ, meanW]
 
         return est_pose
